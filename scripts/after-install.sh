3e266a6a1517decc9159dc1ed446276b5ecd6e16
akeshpatil@gmail.com <akeshpatil@gmail.com>
2015-10-22 16:13:30 +0200

    Updated destination directory

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -1,4 +1,4 @@
-mkdir -p /usr/local/mywebapp_new
-cp -R ./* /usr/local/mywebapp_new
+mkdir -p /usr/local/mywebapp
+cp -R target/ /usr/local/mywebapp
 #docker build -t web-app-demo Dockerfile
 #docker run -d -p 80:8080 web-app-demo
