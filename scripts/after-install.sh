f45f3b5401e325d0f1caf0e78d75443cbd585a55
akeshpatil@gmail.com <akeshpatil@gmail.com>
2015-10-22 16:21:51 +0200

    Updated path

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -1,4 +1,4 @@
 mkdir -p /usr/local/mywebapp
-COPY target /usr/local/mywebapp
+cp -R ../target/ROOT.war /usr/local/mywebapp
 #docker build -t web-app-demo Dockerfile
 #docker run -d -p 80:8080 web-app-demo
